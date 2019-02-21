391c21c0cf82feb5e71e570fdf7f2139fbc1418f
aji <aji@gmail.com>
2019-02-21 14:40:21 +0530

    first name

---
+++ 5678.py
@@ -0,0 +1,6 @@
+def sum (a,b):
+	z=a+b
+	print ("sum of %d and %d is "%(a,b),z)
+sum (12,5)
+	
+	
---
+++ win.py
@@ -0,0 +1,7 @@
+def sum (a,b):
+	z=a+b
+	print ("sum of %d and %d is "%(a,b),z)
+sum (12,5)
+	
+	
+5
